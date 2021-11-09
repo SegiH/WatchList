c1221e96bfe0cc5eb98a1321423eb62e27e865b0
Segi Hovav <shovav@gms4sbc.com>
2021-11-09 10:53:33 -0800

    Made filters only appear for the tab that they are relevant to

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataService } from './core/data.service';
-//import { Router, Event, NavigationStart } from '@angular/router';
+import { Location } from "@angular/common";
 
 @Component({
      selector: 'app-root',
      templateUrl: 'app.component.html',
      styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+     currentRoute: string = "";
      readonly recordLimitOptions = [ // Only applied to WatchList not WatchlistItems
           10,
           50,
@@ -15,9 +17,32 @@ export class AppComponent {
           500
      ]
 
-     constructor(public dataService: DataService) { }
+     constructor(public dataService: DataService, private location: Location, private router: Router) { 
+          //console.log(`URL is ${this.router.url}`)
+          router.events.subscribe(val => {
+               if (location.path() != "") {
+                    switch (location.path()) {
+                         case "/tabs/watchlist":
+                              this.currentRoute="WatchList"
+                              break;
+                         case "/tabs/watchlist-items":
+                              this.currentRoute="WatchListItems"
+                              break;
+                    }
+                 //   console.log(`Path: ${location.path()}`)
+                 //this.route = location.path();
+               } 
+             });
+     }
+
+     ngOnInit() {
+          //alert(`URL: ${this.router.url}`)
+     }
 
      reloadData(event) {
+          if (event == null)
+               return;
+
           if (event.target.id == "IMDBURLMissing")
                this.dataService.getWatchListItemsSubscription(null,null); // IMDB URL Missing filter affects WL Items
           else 

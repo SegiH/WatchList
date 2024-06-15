09c08708a7d5baf110411998d6177c7eb71e202e
Segi <shovav@cryptolab.net>
2024-06-14 18:18:11 -0700

    Fix issue with Date prototype that caused build issues

--- src/app/WatchListStats/page.tsx
+++ src/app/WatchListStats/page.tsx
@@ -26,19 +26,6 @@ const useState = require("react").useState;
 import { DataContext, DataContextType } from "../data-context";
 
 import "./watchliststats.css";
-import { watch } from 'fs/promises';
-
-Date.prototype.getWeek = function () {
-     var date = new Date(this.getTime());
-     date.setHours(0, 0, 0, 0);
-     // Thursday in current week decides the year.
-     date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-     // January 4 is always in week 1.
-     var week1 = new Date(date.getFullYear(), 0, 4);
-     // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-     return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-          - 3 + (week1.getDay() + 6) % 7) / 7);
-}
 
 export default function WatchListStats() {
      const {
@@ -106,6 +93,18 @@ export default function WatchListStats() {
           StartDate: "Start Date",
           EndDate: "End Date",
      };
+
+     const getWeek = () => {
+          var date = new Date();
+          date.setHours(0, 0, 0, 0);
+          // Thursday in current week decides the year.
+          date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+          // January 4 is always in week 1.
+          var week1 = new Date(date.getFullYear(), 0, 4);
+          // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+          return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+               - 3 + (week1.getDay() + 6) % 7) / 7);
+     }
 
      // Initiate start of fetching WatchList Movie Top 10 Stats
      useEffect(() => {
@@ -448,7 +447,7 @@ export default function WatchListStats() {
 
           const movieWeekGrouping: any = [];
 
-          const lastIndex = watchListWeeklyCurrentMovieYearStat === new Date().getFullYear() ? new Date().getWeek() : 52;
+          const lastIndex = watchListWeeklyCurrentMovieYearStat === new Date().getFullYear() ? getWeek() : 52;
           setWatchListWeeklyMovieMaxWeek(lastIndex);
 
           for (let i = 1; i <= lastIndex; i++) {
@@ -474,7 +473,7 @@ export default function WatchListStats() {
 
           const tvWeekGrouping: any = [];
 
-          const lastIndex = watchListWeeklyCurrentTVYearStat === new Date().getFullYear() ? new Date().getWeek() : 52;
+          const lastIndex = watchListWeeklyCurrentTVYearStat === new Date().getFullYear() ? getWeek() : 52;
           setWatchListWeeklyTVMaxWeek(lastIndex);
 
           for (let i = 1; i <= lastIndex; i++) {

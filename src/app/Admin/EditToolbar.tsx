ba387bd6ff11265c2c5e2cefb4ec6536d41033dd
Segi <shovav@cryptolab.net>
2024-07-14 00:48:47 -0700

    Get date with dash seperator from getFormattedDate instead of writing custom date handler

--- src/app/Admin/EditToolbar.tsx
+++ src/app/Admin/EditToolbar.tsx
@@ -23,6 +23,7 @@ const EditToolbar = ({ section, setRowModesModel, setShowActiveBugLogs, showActi
 
      const {
           bugLogs,
+          getFormattedDate,
           isAdding,
           isEditing,
           setBugLogs,
@@ -41,8 +42,8 @@ const EditToolbar = ({ section, setRowModesModel, setShowActiveBugLogs, showActi
 
           switch (section) {
                case "User":
-                    focusField = "Username";
                     nextID = Math.max(...users.map((user: typeof IUser) => user.UserID)) + 1;
+                    focusField = "Username";
                     setUsers((oldRows: typeof IUser) => [...oldRows, { UserID: nextID, Username: "", Password: "", Realname: "", Admin: false, Enabled: true, isNew: true }]);
                     break;
                case "Source":
@@ -59,12 +60,7 @@ const EditToolbar = ({ section, setRowModesModel, setShowActiveBugLogs, showActi
                     nextID = Math.max(...bugLogs.map((bugLog: typeof IBugLog) => bugLog.WLBugID)) + 1;
                     focusField = "WLBugName";
 
-                    const date = new Date();
-
-                    const year = date.getFullYear();
-                    const month = String(date.getMonth() + 1).padStart(2, '0');
-                    const day = String(date.getDate()).padStart(2, '0');
-                    const formattedDate = `${year}-${month}-${day}`;
+                    const formattedDate = getFormattedDate(null, "-");
 
                     setBugLogs((oldRows: typeof IBugLog) => [...oldRows, { WLBugID: nextID, WLBugName: "", AddDate: formattedDate, isNew: true }]);
                     break;

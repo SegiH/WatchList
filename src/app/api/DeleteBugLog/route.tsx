c2013295e8ac65041dc325f988fa8d5005f1d260
Segi <segihovav@gmail.com>
2025-05-13 20:52:15 -0700

    Validate BugLogID in /api/DeleteBugLog

--- src/app/api/DeleteBugLog/route.tsx
+++ src/app/api/DeleteBugLog/route.tsx
@@ -11,13 +11,22 @@ export async function PUT(request: NextRequest) {
      }
 
      if (bugLogId === null) {
-          return Response.json(["ERROR", "ID was not provided"])
+          return Response.json(["ERROR", "ID was not provided"]);
      }
 
      try {
           const db = getDB();
 
           const buglogsDB = db.BugLogs;
+
+          // Validate that BugLog ID provided is valid
+          const validBugLogIDResult = buglogsDB.filter((currentBuglog: IBugLog) => {
+               return String(currentBuglog.BugLogId) === String(bugLogId)
+          });
+
+          if (validBugLogIDResult.length !== 1) {
+               return Response.json(["ERROR", "ID is not valid"]);
+          }
 
           const newBugLogs = buglogsDB.filter((currentBuglog: IBugLog) => {
                return String(currentBuglog.BugLogId) !== String(bugLogId)

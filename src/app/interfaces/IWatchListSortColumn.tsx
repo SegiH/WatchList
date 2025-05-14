0ef8a468fbc63cf6bd188d7b89a773f178e3a329
Segi <segihovav@gmail.com>
2025-05-13 21:20:29 -0700

    Replace multi line comments with single line comment

--- src/app/interfaces/IWatchListSortColumn.tsx
+++ src/app/interfaces/IWatchListSortColumn.tsx
@@ -1,4 +1,4 @@
-/* Used for WatchListSortColumns and WatchListItemsSortColumns. WLI Sort columns do not have start and end date so they have to be optional */
+// Used for WatchListSortColumns and WatchListItemsSortColumns. WLI Sort columns do not have start and end date so they have to be optional
 export default interface IWatchListSortColumn {
      ID: string,
      Name: string,

6d91fddfad91d0e1c668fa6afb4365aad2da2422
Segi H <shovav@cryptolab.net>
2025-10-26 17:56:45 -0700

    Make sure IMDB_Poster is not an empty string

--- src/app/Items/WatchListItemCard.tsx
+++ src/app/Items/WatchListItemCard.tsx
@@ -49,8 +49,8 @@ export default function WatchListItemCard({ currentWatchListItem }: WatchListCar
 
             <a className="show-link" onClick={() => openDetailClickHandler(currentWatchListItem?.WatchListItemID, "Items")}>
                 <div className="clickable">
-                    {currentWatchListItem?.IMDB_Poster !== null && currentWatchListItem?.IMDB_Poster_Error !== true &&
-                        <Image width="128" height="187" alt={currentWatchListItem?.WatchListItemName} src={currentWatchListItem?.IMDB_Poster ?? currentWatchListItem?.IMDB_Poster ?? ""} onError={() => showDefaultSrc(currentWatchListItem.WatchListItemID)} />
+                    {currentWatchListItem?.IMDB_Poster !== null && currentWatchListItem?.IMDB_Poster !== "" && currentWatchListItem?.IMDB_Poster_Error !== true &&
+                        <Image width="128" height="187" alt={currentWatchListItem?.WatchListItemName} src={currentWatchListItem.IMDB_Poster} onError={() => showDefaultSrc(currentWatchListItem.WatchListItemID)} />
                     }
 
                     {(currentWatchListItem?.IMDB_Poster === null || currentWatchListItem?.IMDB_Poster_Error === true) && <>{BrokenImageIconComponent}</>}

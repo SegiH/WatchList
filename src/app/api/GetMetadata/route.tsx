1b0f4c70b878045b97ef49f041c4620519396ed2
Segi <johndoe2k4haha@gmail.com>
2025-12-29 19:21:50 -0800

    Remove unnecessary use of call to getRapidAPIKey()

--- src/app/api/GetMetadata/route.tsx
+++ src/app/api/GetMetadata/route.tsx
@@ -1,15 +1,9 @@
 import { NextRequest } from 'next/server';
-import { generateMetaData, getRapidAPIKey, isLoggedIn } from '../lib';
+import { generateMetaData, isLoggedIn } from '../lib';
 
 export async function GET(request: NextRequest) {
     if (!isLoggedIn(request)) {
         return Response.json(["ERROR", "Error. Not signed in"]);
-    }
-
-    const rapidapi_key = await getRapidAPIKey();
-
-    if (rapidapi_key === "") {
-        return Response.json(["OK", []]);
     }
 
     const metaData = await generateMetaData();
